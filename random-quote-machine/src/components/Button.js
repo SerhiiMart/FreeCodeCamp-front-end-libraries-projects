a149a28626ebed92408fe9517006020bd703b344
Serhii <malgothsm@gmail.com>
2021-04-07 17:12:57 +0300

    Starting project Random Quote Machine from scrach

--- random-quote-machine/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button({buttonDispName, clickHandler}) {
-  return (
-    <button onClick={clickHandler}>{buttonDispName}</button>
-  )
-}
-
-export default Button
