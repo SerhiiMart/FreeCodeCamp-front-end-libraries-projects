14e359021eb696e47723a7a05aa2ddd3a308e340
Serhii <malgothsm@gmail.com>
2021-04-07 21:06:11 +0300

    Finished tehnical side. Need to style it

--- random-quote-machine/app.js
+++ random-quote-machine/app.js
@@ -1,18 +1,52 @@
 const App = () => {
   
 
-  const [quotes, setQuotes] = React.useState('');
-  const [rquotes, setRquotes] = React.useState('');
+  const [quotes, setQuotes] = React.useState([]);
+  const [rQuote, setRquote] = React.useState("");
 
-  React.useEffect(() => {
+  React.useEffect (() => {
     async function fetchData (){
-      const response = await fetch ('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
+      const response = await fetch ('https://type.fit/api/quotes');
+      const data = await response.json();
+      setQuotes(data);
+      let randomIndex = Math.floor(Math.random() * data.length );
+      setRquote(data[randomIndex])
     }
-  }, [input])
+    fetchData();
+  }, [])
 
+  const getNextQuote = () => {
+    let randomIndex = Math.floor(Math.random() * quotes.length );
+      setRquote(quotes[randomIndex])
+  }
 
   return (
-    <div>Something</div>
+    <div className= "container pt-5">
+      <div id="quote-box" className="mb-5">
+          <div className="card">
+            <div className="card-header">Quotes</div>
+            <div className="card-body">
+              {rQuote ? (
+                <>
+                <p id="text" className="card-text">"{rQuote.text}"</p>
+                <h6 id="author" className="card-title">-{rQuote.author || "*Author unknown*"}</h6>
+                </>
+              ) : (
+                <h3> Hold a second! </h3>) }
+                <div className="row">
+                  <button id="new-quote" className="btn btn-primary ml-4 mt-2 w-25" onClick={getNextQuote}>Next Quote</button>
+                  <a target="_blank" id="tweet-quote" href="https://twitter.com/intent/tweet" className="btn btn-warning">
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                  </a>
+                  <a target="_blank" href="https://www.tumblr.com/" className="btn btn-warning">
+                  <i class="fa fa-tumblr" aria-hidden="true"></i>
+                  </a>
+                </div>
+            </div>
+          </div>
+      </div>
+    </div>
+    
   );
 }
 
