0c95cacc8617ccad27a983d2953bd960febb2016
Serhii <malgothsm@gmail.com>
2021-04-08 13:29:46 +0300

    added change of colors and now styling

--- random-quote-machine/app.js
+++ random-quote-machine/app.js
@@ -3,6 +3,7 @@ const App = () => {
 
   const [quotes, setQuotes] = React.useState([]);
   const [rQuote, setRquote] = React.useState("");
+  const [color, setColor] = React.useState("#8A2BE2");
 
   React.useEffect (() => {
     async function fetchData (){
@@ -16,13 +17,28 @@ const App = () => {
   }, [])
 
   const getNextQuote = () => {
+    const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+    
+
     let randomIndex = Math.floor(Math.random() * quotes.length );
+    let randomColor = Math.floor(Math.random() * colors.length );
       setRquote(quotes[randomIndex])
+      setColor(colors[randomColor])
   }
 
   return (
-    <div className= "container pt-5">
-      <div id="quote-box" className="mb-5">
+  <section id="App" style={{backgroundColor: color, minHeight: "100vh"}}>
+    <div id="quote-box" className= "container pt-5">
+      <div className="mb-5">
           <div className="card">
             <div className="card-header">Quotes</div>
             <div className="card-body">
@@ -35,10 +51,10 @@ const App = () => {
                 <h3> Hold a second! </h3>) }
                 <div className="row">
                   <button id="new-quote" className="btn btn-primary ml-4 mt-2 w-25" onClick={getNextQuote}>Next Quote</button>
-                  <a target="_blank" id="tweet-quote" href="https://twitter.com/intent/tweet" className="btn btn-warning">
+                  <a target="_blank" id="tweet-quote" href="https://twitter.com/intent/tweet" className="btn btn-warning ">
                     <i class="fa fa-twitter" aria-hidden="true"></i>
                   </a>
-                  <a target="_blank" href="https://www.tumblr.com/" className="btn btn-warning">
+                  <a target="_blank" href="https://www.tumblr.com/" className="btn btn-warning ">
                   <i class="fa fa-tumblr" aria-hidden="true"></i>
                   </a>
                 </div>
@@ -46,7 +62,7 @@ const App = () => {
           </div>
       </div>
     </div>
-    
+    </section>
   );
 }
 
