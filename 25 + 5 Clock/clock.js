a390cdd260bf8fb3ad313eae8817f8264fb91705
Serhii <malgothsm@gmail.com>
2021-04-16 19:25:13 +0300

    Two more things I need to fix in timer

--- 25 + 5 Clock/clock.js
+++ 25 + 5 Clock/clock.js
@@ -1,27 +1,50 @@
-const Clock = () => {
- 
-  const [fortime, setFortime] = React.useState(25*60);
-  const [stopTime, setStopTime] = React.useState(5*60);
-  const [sessionTime, setSessionTime] = React.useState(25*60);
+
+function Clock () {
+  const soundS = "http://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg";
+  const [fortime, setFortime] = React.useState(25 * 60);
+  const [stopTime, setStopTime] = React.useState(5 * 60);
+  const [sessionTime, setSessionTime] = React.useState(25 * 60);
   const [timeron, setTimeron] = React.useState(false);
   const [onStop, setOnStop]= React.useState(false);
-  const [whistle, setShistle] = React.useState(new Audio('./whistle.mp3'));
+  // const [whistle, setShistle] = React.useState(new Audio('./whistle.mp3'));
+
+  const playWhistle = () => {
+    player.currentTime = 0;
+    player.play();
+  };
+
+  let player = React.useRef(null);
+
+  React.useEffect(() => {
+    if (fortime <= 0) {
+      setOnStop(true);
+      playWhistle();
+    } else if (!timeron && fortime === stopTime) {
+      setOnStop(false);
+    }
+  }, [fortime, onStop, timeron, stopTime, sessionTime]);
+
+  const formatDisplayTime = (time) => {
+    let mins = Math.floor(time / 60);
+    let secs = time % 60;
+    return (
+      (mins < 10 ? "0" + mins : mins) + ":" + (secs < 10 ? "0" + secs : secs)
+    );
+  };
 
   const timeFormat = (time) => {
-    let mins = Math.floor(time/60);
-    let secd = time % 60;
-    return (mins < 10 ? '0' + mins : mins) + ':' + (secd < 10 ? '0' + secd : secd);
+    return time / 60;
   };
 
   const changeTime = (num, type) => {
-    if (type == "break") { 
-      if (stopTime <=60 && num < 0) {
+    if (type === "break") { 
+      if ((stopTime <= 60 && num < 0) || stopTime >= 60 * 60) {
         return;
       }
       setStopTime((prev) => prev + num);
       
     } else {
-      if (sessionTime <= 60 && num < 0){
+      if ((sessionTime <= 60 && num < 0) || sessionTime >= 60 * 60){
         return;
       }
       setSessionTime((prev) => prev + num);
@@ -29,27 +52,6 @@ const Clock = () => {
         setFortime(sessionTime + num);
       }
     };
-  }
-
-  const BreakLength = ({title, changeTime, type, time, timeFormat}) => {
-    return (
-      <section >
-      <h4>{title}</h4>
-      <div>
-        <div id="break-label">
-        <button  id="session-decrement break-decrement" className="btn btn-small waves-effect red accent-4" onClick={()=> changeTime(-60, type)}>
-          <i className="material-icons">arrow_downward</i>  
-        </button>
-        </div>
-        <h4>{timeFormat(time)}</h4>  
-        <div id="session-label">
-        <button id="break-increment session-increment" className="btn btn-small waves-effect red accent-4" onClick={()=> changeTime(60, type)}>
-          <i className="material-icons">arrow_upward</i>  
-        </button>
-        </div>
-      </div>
-      </section>
-    );
   };
 
   const timeControl = () => {
@@ -61,57 +63,75 @@ const Clock = () => {
       let interval = setInterval(() => {
         date = new Date().getTime();
         if (date > nextDate) {
-          setFortime(prev => {
+          setFortime((prev) => {
             if (prev <= 0 && !onStopVar) {
-              playWhistle();
               onStopVar = true;
               setOnStop(true);
               return stopTime;
             } else if (prev <= 0 && onStopVar) {
-              playWhistle();
               onStopVar = false;
               setOnStop(false);
               return sessionTime;
             }
-            return prev -1;
+            return prev - 1;
           })
           nextDate += sec;
         }
-      }, 40);
+      }, 30);
       localStorage.clear();
       localStorage.setItem("interval-id", interval);
     }
-    if(timeron) { clearInterval(localStorage.getItem("interval-id")); }
+    if(timeron) { 
+      clearInterval(localStorage.getItem("interval-id")); 
+    }
     setTimeron(!timeron);
   }
   const timeReset = () => {
-    setFortime(25*60);
-    setStopTime(5*60);
-    setSessionTime(25*60);
-  }
-
-  const playWhistle = () => {
-    whistle.currentTime = 0;
-    whistle.play();
+    clearInterval(localStorage.getItem("interval-id"));
+    setFortime(25 * 60);
+    setStopTime(5 * 60);
+    setSessionTime(25 * 60);
+    player.pause();
+    player.currentTime = 0;
+    setTimeron(false);
+    setOnStop(false);
   }
 
   return (
     <main className="text-center center-align">
       <h1 className="title text-decoration-underline"> 25 + 5 Clock App</h1>
-      <div className="grid">
-          <BreakLength  title={"Break Length"} type="break" changeTime={changeTime} time={stopTime} timeFormat={timeFormat} />
-          <BreakLength   title={"Session Length"} type="session" changeTime={changeTime} time={sessionTime} timeFormat={timeFormat} />
+      <div className="grid row">
+          <BreakLength  title={"Break Length"} type={"break"} changeTime={changeTime} time={stopTime} timeFormat={timeFormat} formatDisplayTime={formatDisplayTime} />
+          <BreakLength   title={"Session Length"} type={"session"} changeTime={changeTime} time={sessionTime} timeFormat={timeFormat} formatDisplayTime={formatDisplayTime} />
       </div>
-        <h3> {onStop ? "Break" : "Session"} </h3>
-      <h1 className="time-format">{timeFormat(fortime)}</h1>
-        <button onClick={timeControl} className="btn-large purple darken-4">
+        <h3 id="timer-label"> {onStop ? "Break" : "Session"} </h3>
+      <h1 id="time-left" className="time-format timer">{formatDisplayTime(fortime)}</h1>
+        <button id="start_stop"  onClick={timeControl} className="btn-large purple darken-4">
           {timeron ? ( <i className="material-icons">pause_circle_filled</i> ) : ( <i className="material-icons">play_circle_filled</i> )}
         </button>
-        <button onClick={timeReset} className="btn-large purple darken-4"><i className="material-icons">autorenew</i></button>
+        <button id="reset" onClick={timeReset} className="btn-large purple darken-4"><i className="material-icons">autorenew</i></button>
+        <audio ref={(g) => (player = g)} src={soundS} id="beep" />
     </main>
   );
 }
 
-
+const BreakLength = ({title, changeTime, type, time, timeFormat}) => {
+  return (
+    <section >
+    <h3 id={type === "break" ? "break-label" : "session-label"}>{title}</h3>
+    <div>
+        <button  id={type === "break" ? "break-decrement" : "session-decrement"}   className="btn btn-small waves-effect red accent-4" onClick={()=> changeTime(-60, type)}>
+          <i className="material-icons">arrow_downward</i>  
+        </button>
+      <h4 className="m-2 display-7"
+        id={type === "break" ? "break-length" : "session-length"}>{timeFormat(time)}
+      </h4>  
+        <button id={type === "break" ? "break-increment" : "session-increment"} className="btn btn-small waves-effect red accent-4" onClick={()=> changeTime(60, type)}>
+          <i className="material-icons">arrow_upward</i>  
+        </button>
+    </div>
+    </section>
+  );
+};
 
 ReactDOM.render(<Clock />, document.getElementById('root'));
