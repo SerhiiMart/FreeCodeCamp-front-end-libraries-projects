e4250e90d39dfc402e3308aba51c18b49139f7af
Serhii <malgothsm@gmail.com>
2021-04-16 14:36:18 +0300

    Saving progress1

--- 25 + 5 Clock/clock.js
+++ 25 + 5 Clock/clock.js
@@ -32,7 +32,7 @@ const Clock = () => {
     return (
       <section >
       <h4>{title}</h4>
-      <div className="grid">
+      <div>
         <button className="btn btn-small waves-effect red accent-4" onClick={()=> changeTime(-60, type)}>
           <i className="material-icons">arrow_downward</i>  
         </button>
@@ -45,14 +45,27 @@ const Clock = () => {
     );
   };
 
+  const timeControl = () => {
 
+  }
+  const timeReset = () => {
+    setFortime(25*60);
+    setStopTime(5*60);
+    setSessionTime(25*60);
+  }
   return (
     <main className="text-center center-align">
       <h1 className="title text-decoration-underline"> 25 + 5 Clock App</h1>
-      <BreakLength title={"Break Length"} type="break" changeTime={changeTime} time={stopTime} timeFormat={timeFormat} />
-      <BreakLength title={"Session Length"} type="session" changeTime={changeTime} time={sessionTime} timeFormat={timeFormat} />
-      <h4>Session</h4>  
-      <div className="time-format">{timeFormat(fortime)}</div>
+      <div className="grid">
+          <BreakLength title={"Break Length"} type="break" changeTime={changeTime} time={stopTime} timeFormat={timeFormat} />
+          <BreakLength title={"Session Length"} type="session" changeTime={changeTime} time={sessionTime} timeFormat={timeFormat} />
+      </div>
+
+      <h1 className="time-format">{timeFormat(fortime)}</h1>
+        <button onClick={timeControl} className="btn-large purple darken-4">
+          {timeron ? ( <i className="material-icons">pause_circle_filled</i> ) : ( <i className="material-icons">play_circle_filled</i> )}
+        </button>
+        <button onClick={timeReset} className="btn-large purple darken-4"><i className="material-icons">autorenew</i></button>
     </main>
   );
 }
